226673135f09a70da45a7ee9ef3bd446c209319d
Ashim Gautam <computer77356@gmail.com>
2025-05-15 18:03:14 +0545

    added new projects

--- src/content/content.js
+++ src/content/content.js
@@ -1,7 +1,26 @@
 
 export const myProjects = [
     {
-        id:1,
+        id: 1,
+        heading: "EduConnect",
+        details: `A full-stack consultancy platform where students can chat in real time with instructors, document managers, and other students. Includes role-based access, group discussions by country, document progress tracking, and video sharing.`,
+        githubLink: 'https://github.com/Ashimgautam356/consultancy',
+        techused: ["Next.js", "Express.js", "MongoDB", "WebSocket", "Prisma","Turborepo"],
+        img: "educonnect.png",
+        liveLink: ''
+    },
+    {
+        id: 2,
+        heading: "QuickPay",
+        details: `A digital wallet-like application where users can send and receive money, view transaction history, and get real-time notifications via polling when money is received. Built to explore secure transactions and backend logic.`,
+        githubLink: 'https://github.com/Ashimgautam356/paytmClone',
+        techused: ["React", "Node.js", "Express", "MongoDB"],
+        img: "quickpay.png",
+        liveLink: 'https://paytmclone-1.onrender.com/'
+    },
+
+    {
+        id:3,
         heading: "Store E-Commerce Website",
         details: ` It is a frontend project. Where I'have created login page
         interface, landing page and have done routing with react
@@ -13,7 +32,7 @@ export const myProjects = [
         liveLink: 'https://incomparable-belekoy-f6ffd1.netlify.app/#/'
     },
     {
-        id:2,
+        id:4,
         heading: "Live Weather",
         details: ` It is a frontend project. Where I'have created login page
         interface, landing page and have done routing with react
@@ -25,7 +44,7 @@ export const myProjects = [
         liveLink: 'https://weather-web-app-two.vercel.app/'
     },
     {
-        id:3,
+        id:5,
         heading: "Todo List",
         details: ` It is a frontend project. Where I'have created login page
         interface, landing page and have done routing with react
